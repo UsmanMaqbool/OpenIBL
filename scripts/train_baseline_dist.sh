4ad26af760ef361f827c7ba5f181eead2f841b54
usmanmaqbool <usmanmaqbool@outlook.com>
2022-09-12 09:24:06 +0800

    upstream-default-working

--- scripts/train_baseline_dist.sh
+++ scripts/train_baseline_dist.sh
@@ -15,15 +15,15 @@ if [ $# -ne 1 ]
     exit 1
 fi
 
-while true # find unused tcp port
-do
-    PORT=$(( ((RANDOM<<15)|RANDOM) % 49152 + 10000 ))
-    status="$(nc -z 127.0.0.1 $PORT < /dev/null &>/dev/null; echo $?)"
-    if [ "${status}" != "0" ]; then
-        break;
-    fi
-done
-PORT = 6010
+# while true # find unused tcp port
+# do
+#     PORT=$(( ((RANDOM<<15)|RANDOM) % 49152 + 10000 ))
+#     status="$(nc -z 127.0.0.1 $PORT < /dev/null &>/dev/null; echo $?)"
+#     if [ "${status}" != "0" ]; then
+#         break;
+#     fi
+# done
+PORT=6010
 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT --use_env \
 examples/netvlad_img.py --launcher pytorch --tcp-port ${PORT} \
   -d ${DATASET} --scale ${SCALE} \
@@ -31,4 +31,4 @@ examples/netvlad_img.py --launcher pytorch --tcp-port ${PORT} \
   --width 640 --height 480 --tuple-size 1 -j 2 --neg-num 10 --test-batch-size 32 \
   --margin 0.1 --lr ${LR} --weight-decay 0.001 --loss-type ${LOSS} \
   --eval-step 1 --epochs 5 --step-size 5 --cache-size 1000 \
-  --logs-dir logs/netVLAD/${DATASET}${SCALE}-${ARCH}/${LAYERS}-${LOSS}-lr${LR}-tuple${GPUS}
+  --logs-dir=/media/leo/2C737A9872F69ECF/why-so-deepv2-data/pittsburgh/netvlad-run/${DATASET}${SCALE}-${ARCH}/${LAYERS}-${LOSS}-lr${LR}-tuple${GPUS}
