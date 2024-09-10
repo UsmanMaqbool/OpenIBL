246bbf5d4488fd3104599fd0c684e9bb4f56c760
Usman Maqbool <usmanmaqbool@outlook.com>
2024-09-10 09:38:33 -0400

    0910-s-2

--- scripts/leo/train_sfrs_slurm_all.sh
+++ scripts/leo/train_sfrs_slurm_all.sh
@@ -47,8 +47,8 @@ SCALE="$4"
 NUMCLUSTER=64
 LAYERS=conv5
 LR=0.001
-TUMPLESIZE=1
-CACHEBS=32
+TUMPLESIZE=4
+CACHEBS=50
 NPOCH=5
 PORT=6010
 
@@ -100,7 +100,7 @@ srun --mpi=pmix_v3 -p=gpu --cpus-per-task=2 -n${GPUS} \
 python -u examples/netvlad_img_sfrs.py --launcher slurm --tcp-port ${PORT} \
   -d ${DATASET} --scale ${SCALE} \
   -a ${ARCH} --layers ${LAYERS} --syncbn \
-  --width 640 --height 480 --tuple-size 1 -j 2 --test-batch-size ${CACHEBS} \
+  --width 640 --height 480 --tuple-size ${TUMPLESIZE} -j 2 --test-batch-size ${CACHEBS} \
   --neg-num 10  --pos-pool 20 --neg-pool 1000 --pos-num 10 \
   --margin 0.1 --lr ${LR} --weight-decay 0.001 --loss-type ${LOSS} --soft-weight 0.5 \
   --eval-step 1 --epochs 5 --step-size 5 --cache-size 1000 --generations 4 --temperature 0.07 0.07 0.06 0.05 --logs-dir ${FILES} --data-dir ${DATASET_DIR} \
@@ -120,7 +120,7 @@ do
   echo "======================================="
   $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT --use_env \
    examples/test_pitts_tokyo.py --launcher pytorch \
-    -a ${ARCH} --test-batch-size 32 -j 2 \
+    -a ${ARCH} --test-batch-size ${CACHEBS} -j 2 \
     --vlad --reduction --method ${METHOD} \
     --resume ${RESUME} --segmentation-head ${SHEADWEIGHTS} \
     --num-clusters ${NUMCLUSTER}
@@ -145,7 +145,7 @@ srun --mpi=pmix_v3 -p=gpu --cpus-per-task=2 -n${GPUS} \
 python -u examples/netvlad_img_sfrs.py --launcher slurm --tcp-port ${PORT} \
   -d ${DATASET} --scale ${SCALE} \
   -a ${ARCH} --layers ${LAYERS} --syncbn \
-  --width 640 --height 480 --tuple-size 1 -j 2 --test-batch-size ${CACHEBS} \
+  --width 640 --height 480 --tuple-size ${TUMPLESIZE} -j 2 --test-batch-size ${CACHEBS} \
   --neg-num 10  --pos-pool 20 --neg-pool 1000 --pos-num 10 \
   --margin 0.1 --lr ${LR} --weight-decay 0.001 --loss-type ${LOSS} --soft-weight 0.5 \
   --eval-step 1 --epochs 5 --step-size 5 --cache-size 1000 --generations 4 --temperature 0.07 0.07 0.06 0.05 --logs-dir ${FILES} --data-dir ${DATASET_DIR} \
@@ -166,7 +166,7 @@ do
   echo "======================================="
   $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT --use_env \
    examples/test_pitts_tokyo.py --launcher pytorch \
-    -a ${ARCH} --test-batch-size 32 -j 2 \
+    -a ${ARCH} --test-batch-size ${CACHEBS} -j 2 \
     --vlad --reduction --method ${METHOD} \
     --resume ${RESUME} --segmentation-head ${SHEADWEIGHTS} \
   --num-clusters ${NUMCLUSTER}
@@ -191,7 +191,7 @@ srun --mpi=pmix_v3 -p=gpu --cpus-per-task=2 -n${GPUS} \
 python -u examples/netvlad_img_sfrs.py --launcher slurm --tcp-port ${PORT} \
   -d ${DATASET} --scale ${SCALE} \
   -a ${ARCH} --layers ${LAYERS} --syncbn \
-  --width 640 --height 480 --tuple-size 1 -j 2 --test-batch-size ${CACHEBS} \
+  --width 640 --height 480 --tuple-size ${TUMPLESIZE} -j 2 --test-batch-size ${CACHEBS} \
   --neg-num 10  --pos-pool 20 --neg-pool 1000 --pos-num 10 \
   --margin 0.1 --lr ${LR} --weight-decay 0.001 --loss-type ${LOSS} --soft-weight 0.5 \
   --eval-step 1 --epochs 5 --step-size 5 --cache-size 1000 --generations 4 --temperature 0.07 0.07 0.06 0.05 --logs-dir ${FILES} --data-dir ${DATASET_DIR} \
@@ -212,7 +212,7 @@ do
   echo "======================================="
   $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT --use_env \
    examples/test_pitts_tokyo.py --launcher pytorch \
-    -a ${ARCH} --test-batch-size 32 -j 2 \
+    -a ${ARCH} --test-batch-size ${CACHEBS} -j 2 \
     --vlad --reduction --method ${METHOD} \
     --resume ${RESUME} --segmentation-head ${SHEADWEIGHTS} \
   --num-clusters ${NUMCLUSTER}
